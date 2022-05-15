9be36846a814f2f38c7317becb3661dd7c87c592
ahnjh <algorithm123@naver.com>
2022-05-15 17:15:57 +0900

    init: project

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import React from "react";
+
+const Root = styled.div`
+    background: red;
+`;
+
+export default function Home() {
+    return (
+        <Root>
+            sdfasdf
+        </Root>
+    )
+}
