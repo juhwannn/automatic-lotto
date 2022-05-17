bc761e16c3400e168f246da4c8d3c28fcfc8f03f
ahnjh <algorithm123@naver.com>
2022-05-17 16:06:21 +0900

    add: index.js api 통신 테스트

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,49 @@
 import styled from "styled-components";
-import React from "react";
+import React, {useEffect} from "react";
+import axios from "axios";
 
 const Root = styled.div`
-    background: red;
+    .mainHeader {
+        background: red;
+            
+        height: 5vh; 
+    }
+    
+    .mainBody {
+        background: green;
+        
+        min-height: 90vh;
+    }
+    
+    .mainFooter {
+        background: blue;
+        
+        height: 5vh;
+    }
 `;
 
 export default function Home() {
+
+    useEffect(() => {
+        (async () => {
+            const response = await axios.get("/api/test");
+            console.log("response : " + JSON.stringify(response.data));
+        })();
+    }, []);
+
     return (
         <Root>
-            sdfasdf
+            <div className="mainHeader">
+                mainHeader
+            </div>
+
+            <div className="mainBody">
+                mainBody
+            </div>
+
+            <div className="mainFooter">
+                mainFooter
+            </div>
         </Root>
     )
 }
